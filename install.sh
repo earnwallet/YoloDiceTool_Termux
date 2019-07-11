a6884ce6b7ee35051120f5106112a367516d89c4
MrCyjaneK <MrCyjaneK@Gmail.com>
2019-07-11 06:36:19 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt update
+apt upgrade -y
+apt install curl nodejs php git -y
+git clone https://github.com/earnwallet/YoloDiceTool_Termux yd
+cd yd
+chmod +x YoloScript.sh
+cd ..
+echo "Complete! to Start script use:\n\n\n\n\n\n\n"
+echo "cd ~yd; sh YoloScript.sh"
